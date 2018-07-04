7a40169c1a0945693f1c98b773f8fc36a0b12b9e
Henrik Bak Thomassen <hbt@peytz.dk>
2018-07-04 13:07:57 +0200

    Article styling etc

--- themes/snakomhash/js/sidebar.js
+++ themes/snakomhash/js/sidebar.js
@@ -6,11 +6,13 @@
       $('a.sidebar-open').click(function() {
         $('.overlay-sidebar').addClass('active');
         $('.layout-sidebar').addClass('active');
+        $('body').addClass('sidebar-open');
       });
 
       $('a.sidebar-close, .overlay-sidebar').click(function () {
         $('.overlay-sidebar').removeClass('active');
         $('.layout-sidebar').removeClass('active');
+        $('body').removeClass('sidebar-open');
       });
 
     }
